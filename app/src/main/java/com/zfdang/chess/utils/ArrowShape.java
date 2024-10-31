c2e3feeb5a80efed8d0d7ef67ad865d24ff3c9d2
dantifer <dantifer@gmail.com>
2024-10-31 11:21:57 +0800

    refine ArrowShape

--- app/src/main/java/com/zfdang/chess/utils/ArrowShape.java
+++ app/src/main/java/com/zfdang/chess/utils/ArrowShape.java
@@ -3,9 +3,16 @@ package com.zfdang.chess.utils;
 import android.graphics.Matrix;
 import android.graphics.Path;
 
+/*
+ * ArrowShape.java
+ * 设置箭头的参数，然后生成箭头的Path
+ * 具体的参数可以参考这个文档：
+ * https://github.com/zfdang/chinese-chess-android/blob/master/docs/%E7%BB%98%E5%88%B6%E7%AE%AD%E5%A4%B4.md
+ * Created by zfdang on 2018-03-25
+ */
 public class ArrowShape {
-    float angle; // the angle degree of the arrow, the narrower the sharper
-    float innerAngle; // the inner angle degree of the arrow, the narrower the sharper
+    float angle;
+    float innerAngle;
     float sideLength;
     float headWidth;
     float tailWidth;
@@ -26,21 +33,16 @@ public class ArrowShape {
         this.tailWidth = 12;
     }
 
-    public void getTransformedPath(Path path, float _x1, float _y1, float _x2, float _y2) {
+    public void getTransformedPath(Path path, float x0, float y0, float x1, float y1) {
 
         double angle1 = this.angle / 360 * Math.PI;
         double angle2 = this.innerAngle / 360 * Math.PI;
-        float tanv2 = (float)Math.tan(angle2);
-        float sinv1 = (float)Math.sin(angle1);
-        float cosv1 = (float)Math.cos(angle1);
-
-        float x0 = _x1;
-        float y0 = _y1;
-        float x1 = _x2;
-        float y1 = _y2;
+        float sinv1 = (float) Math.sin(angle1);
+        float cosv1 = (float) Math.cos(angle1);
+        float tanv2 = (float) Math.tan(angle2);
 
         // 直接设置箭头的长度为目标长度，避免缩放时箭头变形
-        float x2 = (float)Math.hypot(x1 - x0, y1 - y0);
+        float x2 = (float) Math.hypot(x1 - x0, y1 - y0);
         float y2 = 0;
 
         // (x3, y3)
@@ -69,7 +71,5 @@ public class ArrowShape {
         mtx.postRotate((float) (Math.atan2(y1 - y0, x1 - x0) * 180 / Math.PI));
         mtx.postTranslate(x0, y0);
         path.transform(mtx);
-
-        return;
     }
 }
