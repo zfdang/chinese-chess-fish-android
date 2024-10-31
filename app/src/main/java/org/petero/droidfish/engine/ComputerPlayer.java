630ba286c5f703f3463f2ce04410473105b7dd28
dantifer <dantifer@gmail.com>
2024-10-31 17:47:28 +0800

    remove elo data

--- app/src/main/java/org/petero/droidfish/engine/ComputerPlayer.java
+++ app/src/main/java/org/petero/droidfish/engine/ComputerPlayer.java
@@ -133,7 +133,6 @@ public class ComputerPlayer {
         int movesToGo;          // Number of moves to next time control
 
         String engine;          // Engine name (identifier)
-        int elo;                // Engine UCI_Elo setting, or Integer.MAX_VALUE for full strength
         int numPV;              // Number of PV lines to compute
 
         boolean ponderEnabled;  // True if pondering enabled, for engine time management
@@ -171,14 +170,13 @@ public class ComputerPlayer {
          * @param ponderEnabled True if pondering is enabled in the GUI. Can affect time management.
          * @param ponderMove    Move to ponder, or null for non-ponder search.
          * @param engine        Chess engine to use for searching.
-         * @param elo           Engine Elo strength setting.
          */
         public static SearchRequest searchRequest(int id, long now,
                                                   Position prevPos, ArrayList<Move> mList,
                                                   Position currPos, boolean drawOffer,
                                                   int wTime, int bTime, int wInc, int bInc, int movesToGo,
                                                   boolean ponderEnabled, Move ponderMove,
-                                                  String engine, int elo) {
+                                                  String engine) {
             SearchRequest sr = new SearchRequest();
             sr.searchId = id;
             sr.startTime = now;
@@ -194,7 +192,6 @@ public class ComputerPlayer {
             sr.bInc = bInc;
             sr.movesToGo = movesToGo;
             sr.engine = engine;
-            sr.elo = elo;
             sr.numPV = 1;
             sr.ponderEnabled = ponderEnabled;
             sr.ponderMove = ponderMove;
@@ -231,7 +228,6 @@ public class ComputerPlayer {
             sr.isAnalyze = true;
             sr.wTime = sr.bTime = sr.wInc = sr.bInc = sr.movesToGo = 0;
             sr.engine = engine;
-            sr.elo = Integer.MAX_VALUE;
             sr.numPV = numPV;
             sr.ponderEnabled = false;
             sr.ponderMove = null;
@@ -301,7 +297,7 @@ public class ComputerPlayer {
             if (o != null)
                 o.setFromString(e.getValue());
         }
-        Log.d("DroidComputerPlayer", "getUCIOptions: " + opts.toString());
+        Log.d("ComputerPlayer", "getUCIOptions: " + opts.toString());
         return opts;
     }
 
@@ -348,63 +344,8 @@ public class ComputerPlayer {
         }
     }
 
-    public static class EloData {
-        public boolean limitStrength = false; // True if engine strength reduction is enabled
-        public int elo = 0;                   // Current strength setting
-        public int minElo = 0;                // Smallest possible Elo value
-        public int maxElo = 0;                // Largest possible Elo value
 
-        /**
-         * Return true if engine is able to change the playing strength.
-         */
-        public boolean canChangeStrength() {
-            return minElo < maxElo;
-        }
 
-        /**
-         * Get current Elo setting.
-         * Return MAX_VALUE if reduced strength not enabled or not supported.
-         */
-        public int getEloToUse() {
-            if (canChangeStrength() && limitStrength)
-                return elo;
-            return Integer.MAX_VALUE;
-        }
-    }
-
-    /**
-     * Return engine Elo strength data.
-     */
-    public synchronized EloData getEloData() {
-        EloData ret = new EloData();
-        UCIEngine uci = uciEngine;
-        if (uci != null) {
-            UCIOptions opts = uci.getUCIOptions();
-            UCIOptions.OptionBase lsOpt = opts.getOption("UCI_LimitStrength");
-            UCIOptions.OptionBase eloOpt = opts.getOption("UCI_Elo");
-            if (lsOpt instanceof UCIOptions.CheckOption &&
-                    eloOpt instanceof UCIOptions.SpinOption) {
-                ret.limitStrength = ((UCIOptions.CheckOption) lsOpt).value;
-                UCIOptions.SpinOption eloSpin = (UCIOptions.SpinOption) eloOpt;
-                ret.elo = eloSpin.value;
-                ret.minElo = eloSpin.minValue;
-                ret.maxElo = eloSpin.maxValue;
-            }
-        }
-        return ret;
-    }
-
-    /**
-     * Set engine UCI strength parameters.
-     */
-    public void setStrength(int elo) {
-        Map<String, String> opts = new TreeMap<>();
-        boolean limitStrength = elo != Integer.MAX_VALUE;
-        opts.put("UCI_LimitStrength", limitStrength ? "true" : "false");
-        if (limitStrength)
-            opts.put("UCI_Elo", String.valueOf(elo));
-        setEngineUCIOptions(opts);
-    }
 
     /** Return all book moves, both as a formatted string and as a list of moves. */
 //    public final Pair<String, ArrayList<Move>> getBookHints(BookPosInput posInput,
@@ -443,7 +384,7 @@ public class ComputerPlayer {
             uciEngine.writeLineToEngine("ponderhit");
             engineState.setState(MainState.SEARCH);
             pvModified = true;
-            notifyGUI();
+            notifyListener();
         }
     }
 
@@ -692,7 +633,6 @@ public class ComputerPlayer {
 
         // Set strength and MultiPV parameters
         clearInfo();
-        uciEngine.setEloStrength(searchRequest.elo);
         if (maxPV > 1) {
             int num = Math.min(maxPV, searchRequest.numPV);
             uciEngine.setOption("MultiPV", num);
@@ -801,7 +741,7 @@ public class ComputerPlayer {
             if (Thread.currentThread().isInterrupted())
                 return;
             String s = uci.readLineFromEngine(timeout);
-            Log.d("DroidComputerPlayer", "monitorLoop: " + s);
+            Log.d("ComputerPlayer", "monitorLoop: " + s);
             long t0 = System.currentTimeMillis();
             while (s != null && !s.isEmpty()) {
                 if (Thread.currentThread().isInterrupted())
@@ -817,7 +757,7 @@ public class ComputerPlayer {
             processEngineOutput(uci, s);
             if (Thread.currentThread().isInterrupted())
                 return;
-            notifyGUI();
+            notifyListener();
             if (Thread.currentThread().isInterrupted())
                 return;
         }
@@ -1158,7 +1098,7 @@ public class ComputerPlayer {
     /**
      * Notify GUI about search statistics.
      */
-    private synchronized void notifyGUI() {
+    private synchronized void notifyListener() {
         if (Thread.currentThread().isInterrupted())
             return;
 
