f99832bb7535920e5f60d3d50501ab9e7fe5b89d
dantifer <dantifer@gmail.com>
2024-11-09 20:02:54 +0800

    impl setting dialog

--- app/src/main/java/com/zfdang/chess/SettingDialogFragment.kt
+++ app/src/main/java/com/zfdang/chess/SettingDialogFragment.kt
@@ -4,9 +4,10 @@ import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.widget.CheckBox
-import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.SeekBar
-import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 
@@ -24,15 +25,72 @@ class SettingDialogFragment : DialogFragment() {
         val inflater = requireActivity().layoutInflater
         val view: View = inflater.inflate(R.layout.setting_dialog, null)
 
-        val seekBarInput = view.findViewById<SeekBar>(R.id.number_seekbar)
-        val booleanInput = view.findViewById<CheckBox>(R.id.boolean_input)
+        // bind all items in setting_dialog.xml
+        val booleanSound = view.findViewById<CheckBox>(R.id.boolean_sound)
+
+
+        val historyInput = view.findViewById<SeekBar>(R.id.seekbar_history)
+        val historyText = view.findViewById<TextView>(R.id.textview_history)
+
+        val radioDepth = view.findViewById<RadioButton>(R.id.radioButton_depth)
+        val depthInput = view.findViewById<SeekBar>(R.id.seekBar_depth)
+        val depthText = view.findViewById<TextView>(R.id.textView_depth)
+
+
+        val radioTime = view.findViewById<RadioButton>(R.id.radioButton_time)
+        val timeInput = view.findViewById<SeekBar>(R.id.seekBar_time)
+        val timeText = view.findViewById<TextView>(R.id.textView_time)
+
+        val radioInfinite = view.findViewById<RadioButton>(R.id.radioButton_infinite)
+
+        historyInput.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                historyText.text = "$progress"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+            }
+        })
+        depthInput.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                depthText.text = "$progress"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+            }
+        })
+        timeInput.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                timeText.text = "$progress"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+            }
+        })
+
+
 
         builder.setView(view)
-            .setTitle("Set Values")
-            .setPositiveButton("OK") { dialog, id ->
-                val booleanValue = booleanInput.isChecked
+            .setTitle("游戏设置")
+            .setPositiveButton("确定") { dialog, id ->
+                val booleanValue = booleanSound.isChecked
             }
-            .setNegativeButton("Cancel") { dialog, id ->
+            .setNegativeButton("取消") { dialog, id ->
                 listener?.onDialogNegativeClick()
                 dialog.cancel()
             }
