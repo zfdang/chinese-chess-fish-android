690b3afb9cb46b80d305da80c61b2fbdd6b02524
dantifer <dantifer@gmail.com>
2024-11-12 11:06:49 +0800

    impl move history

--- app/src/main/java/com/zfdang/chess/adapters/MoveHistoryAdapter.kt
+++ app/src/main/java/com/zfdang/chess/adapters/MoveHistoryAdapter.kt
@@ -15,12 +15,19 @@ class MoveHistoryAdapter(private val context: Context, private val tableLayout: 
         // add item in game.historyRecords to tableLayout in reverse order
         var moveCount = 1
         val game = controller.game
-        for (item in game.history) {
+        for(i in 0 until game.history.size step 2) {
+            val item = game.history.get(i)
             val tableRow = LayoutInflater.from(context).inflate(R.layout.history_table_row_item, tableLayout, false) as TableRow
             val col1 = tableRow.findViewById<TextView>(R.id.move_index)
-            val col2 = tableRow.findViewById<TextView>(R.id.move_desc)
+            val col2 = tableRow.findViewById<TextView>(R.id.move_1)
+            val col3 = tableRow.findViewById<TextView>(R.id.move_2)
+
             col1.text = moveCount.toString()
             col2.text = String.format("%s (%s)", item.chsString, item.ucciString)
+            if(i < game.history.size - 1) {
+                val nextItem = game.history.get(i + 1)
+                col3.text = String.format("%s (%s)", nextItem.chsString, nextItem.ucciString)
+            }
             tableLayout.addView(tableRow, 0)
             moveCount ++
         }
