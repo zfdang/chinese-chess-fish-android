a35637130ae4eb64a923ccb5a5e8d6d969676cda
dantifer <dantifer@gmail.com>
2024-11-12 12:51:57 +0800

    refine updateapk.sh

--- updateApk.sh
+++ updateApk.sh
@@ -9,18 +9,18 @@ cp app/build/outputs/apk/release/*.apk docs/apk/
 ## find apk filename
 cd ./docs/apk/
 APK="$(ls *.apk)"
-echo $APK
 git lfs track "*.apk"
+echo $APK
 
 ## grep file
 cd ../
 OLDAPK="$(grep -o -E -i 'ChessFish_\d{8}_\d{3}_release\.apk' release.html | head -1)"
-
 echo $OLDAPK
 
 echo "update release.html"
 sed -i -e "s/$OLDAPK/$APK/g" release.html
 
+echo "push"
 git add .
-git commit -a -m "update docs"
+git commit -a -m "update apk"
 git push
